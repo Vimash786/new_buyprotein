221568b064eadb4ede233098e894ba258b1faee2
vimash prajapati <vimashprajapati03@gmail.com>
2025-06-23 22:46:11 +0530

    make seller, product, and orders -mfs

---
+++ app/Models/sellers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class sellers extends Model
+{
+    /** @use HasFactory<\Database\Factories\SellersFactory> */
+    use HasFactory;
+}
